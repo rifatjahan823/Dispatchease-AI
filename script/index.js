9028290cf105bc20731cd526075258106b26f0d0
rifatjahan823 <rifatjahan823@gmail.com>
2024-01-14 15:52:17 +0600

    first commit

---
+++ script/index.js
@@ -0,0 +1,5 @@
+
+function toggleSearch() {
+    var searchBar = document.getElementById('searchBar');
+    searchBar.style.display = (searchBar.style.display === 'none' || searchBar.style.display === '') ? 'block' : 'none';
+}
