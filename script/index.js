6da96d23d1fc00958edd2ba2d02c722070de9ca0
rifatjahan823 <rifatjahan823@gmail.com>
2024-01-15 19:34:49 +0600

    d

--- script/index.js
+++ script/index.js
@@ -3,11 +3,4 @@ function toggleSearch() {
     var searchBar = document.getElementById('searchBar');
     searchBar.style.display = (searchBar.style.display === 'none' || searchBar.style.display === '') ? 'block' : 'none';
 }
-    // Get the object element
-    var dynamicObject = document.getElementById("dynamicObject");
-
-    // Add an event listener to adjust the height when the content is loaded
-    dynamicObject.addEventListener("load", function() {
-      // Set the height of the object element based on the content height
-      dynamicObject.style.height = dynamicObject.contentDocument.body.scrollHeight + "px";
-    });
+ 
